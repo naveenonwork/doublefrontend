775552b5e02f7d1af5cf765be589a6812defe7a3
naveen <naveen15aug1982@gmail.com>
2023-06-09 07:45:17 +0530

    proxy settings change

--- vite.config.js
+++ vite.config.js
@@ -3,34 +3,33 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import https from "https";
 import react from "@vitejs/plugin-react";
+ 
 //https://doubledeploy.vercel.app
 //http://127.0.0.1:${process.env.BACKEND_PORT}
 const proxyOptions = {
   target: `https://double-backend.onrender.com`,
-  changeOrigin: true,
-  secure: true,
-  ws: false,
+  changeOrigin: true 
 };
 
 const host = process.env.HOST
-  ? process.env.HOST 
-  : "localhost";
+? process.env.HOST.replace(/https?:\/\//, "")
+: "localhost";
 
 let hmrConfig;
 if (host === "localhost") {
-  hmrConfig = {
-    protocol: "ws",
-    host: "localhost",
-    port: 64999,
-    clientPort: 64999,
-  };
+hmrConfig = {
+  protocol: "ws",
+  host: "localhost",
+  port: 64999,
+  clientPort: 64999,
+};
 } else {
-  hmrConfig = {
-    protocol: "ws",
-    host: "localhost",
-    port: 64999,
-    clientPort: 64999,
-  };
+hmrConfig = {
+  protocol: "wss",
+  host: host,
+  port: process.env.FRONTEND_PORT,
+  clientPort: 443,
+};
 }
 
 export default defineConfig({
@@ -42,7 +41,7 @@ export default defineConfig({
   },
   server: {
     
-    
+    server: { https: true },
     proxy: {
       "^/(\\?.*)?$": proxyOptions,
       "^/api(/|(\\?.*)?$)": proxyOptions,
