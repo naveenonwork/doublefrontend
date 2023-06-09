a8b5e89463ce01e158cfc901818543dbcc47d71f
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2023-06-09 13:40:05 +0200

    changes for promo video#1

--- components/3d/Showroom2.jsx
+++ components/3d/Showroom2.jsx
@@ -1,9 +1,7 @@
 import React, { Suspense, useEffect, useMemo, useRef, useState } from "react";
-import { KTX2Loader } from "three-stdlib";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Loader, useGLTF as useGLTFImpl } from "@react-three/drei";
 
-const kTX2Loader = new KTX2Loader();
 
 function Showroom(props) {
   const ref1 = useRef();
@@ -101,13 +99,6 @@ function Showroom(props) {
 
 export default Showroom;
 
-function useGLTF(url) {
-  const gl = useThree((state) => state.gl);
-  return useGLTFImpl(url, true, true, (loader) => {
-    kTX2Loader.detectSupport(gl);
-    kTX2Loader.setTranscoderPath("/basis/");
-    loader.setKTX2Loader(kTX2Loader);
-  });
-}
+
 
 // useGLTFImpl.preload( "/bandanaShirtGreen.glb")
